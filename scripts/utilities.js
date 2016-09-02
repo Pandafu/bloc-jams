b599a2d18b4e2a033033563af2db3f93952c9d5f
riss <rissgadi@gmail.com>
2016-09-02 16:26:47 -0500

    correction to the assignment

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,15 +1,5 @@
-function forEACH(array){
-   
-    var revealPoint = function(index){
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
-    
-        points.forEach(revealPoint);
-};
-
-
-
-/* Replace the for loop in the animatePoints function with a forEach block and confirm that the selling points still animate properly. */
+function forEach(array, callback){
+    for(var i = 0; i < array.length; i++){
+        callback(array[i]);
+    }
+}
